f69bf2418d1b8e6c47aea5582350af832d9373fd
MajoritySky2496 <gzag2496@gmail.com>
2023-09-13 11:32:17 +0500

    filters#100 1. Убрал подчеркивание в дата классе 2. Вместо пары создал отдельный класс 3. Поменял заливку 4. Обработал большой текст 5. Перемести Другие регоины на последнее место

--- app/src/main/java/ru/practicum/android/diploma/filters/domain/FiltersInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/domain/FiltersInteractor.kt
@@ -3,12 +3,13 @@ import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.filters.domain.models.Areas
 import ru.practicum.android.diploma.filters.domain.models.Filters
 import ru.practicum.android.diploma.filters.domain.models.Industry
+import ru.practicum.android.diploma.util.Results
 
 interface FiltersInteractor {
-    suspend fun getAreas(): Flow<Pair<List<Areas>?, String?>>
+    suspend fun getAreas(): Flow<Results<List<Areas>?, String?>>
 
     suspend fun getFilters():Flow<Filters>
 
     suspend fun writeFilters(filters: Filters)
-    suspend fun getIndustries():Flow<Pair<List<Industry>?, String?>>
+    suspend fun getIndustries():Flow<Results<List<Industry>?, String?>>
 }
